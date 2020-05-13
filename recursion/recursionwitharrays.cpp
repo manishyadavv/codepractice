6a96e9bcbf522f94e9e8067c8940f097c649dee7
manishyadavv <manishyadav.nd@gmail.com>
2020-05-13 22:49:20 +0530

    max in array and linear search in array

--- recursion/recursionwitharrays.cpp
+++ recursion/recursionwitharrays.cpp
@@ -21,10 +21,45 @@ int recursionwitharrays(int arr[],int size)
 
 }
 
+//Expectation=> print champion from arr we want
+// Faith => we know champion from arr[1]-arr[n],
+//E<=>F arr[1]-arr[n] ka champion agar arr[0] se bada to return champion else return arr[0]
+int printMax(int arr[],int size)
+{
+    if(size==4)
+    {
+     return arr[size];
+    }
+
+    int misa = printMax(arr,size+1);
+
+    if(misa>=arr[size])
+    {return misa;}
+    else
+    { return arr[size];}
+
+}
+int linearSearch(int arr[],int size,int key)
+{
+
+    if(size==5)
+        return 0  ;
+    if(arr[size]==key)
+    {cout<<"Found at"<<" "<<size;
+        return 0;
+    }
+    linearSearch(arr,size+1,key);
+
+    return 0;
+
+}
+
 
 int main()
 {
     int arr[5]={10,20,30,40,50};
-    recursionwitharrays(arr,0);
+    cout<<printMax(arr,0);
+    cout<<endl;
+    linearSearch(arr,0,50);
     return 0;
 }
