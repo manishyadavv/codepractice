8c0b84a7f2a61e4ed31a99dd82acefb99ee25734
manishyadavv <manishyadav.nd@gmail.com>
2020-05-31 14:00:31 +0530

    {3} Nqueens done

--- BADE-SAWAAL/nqueens1.cpp
+++ BADE-SAWAAL/nqueens1.cpp
@@ -10,6 +10,52 @@ using namespace std;
 
 static int counter=0;
 
+
+//we have to check given row,col ke piche ki row piche ka column
+// and piche ka diagonal kyuki combination kr rhe hai
+//aage ki queens aani baaki hai abhi
+//idQueenSafe se bas check kr rhe hai piche kahi koi queen placed to nahi hai
+
+bool isQueenSafe(vector<vector<bool>>& chess,int row,int col)
+{
+    for(int i=col-1;i>=0;i--)
+    {
+        if(chess[row][i]==true)
+        {
+            return false;
+        }
+    }
+
+    for(int i=row-1;i>=0;i--)
+    {
+        if(chess[i][col]==true)
+        {
+            return false;
+        }
+    }
+    //pichla diagonal
+    for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--)
+    {
+        if(chess[i][j]==true)
+        {
+            return false;
+        }
+    }
+    //agla diagonal
+    for(int i=row-1,j=col+1;i>=0&&j<chess[0].size();i--,j++)
+    {
+        if(chess[i][j]==true)
+        {
+            return false;
+        }
+    }
+
+
+    return true;
+
+
+}
+
 //cq=current queen
 //lqi last queen i last queen j
 void nqueen1(vector<vector<bool>>& chess,int cq,int lqi,int lqj,string asf)
@@ -26,9 +72,14 @@ void nqueen1(vector<vector<bool>>& chess,int cq,int lqi,int lqj,string asf)
 
     for(int i=lqi;i<chess.size();i++)
     {
-        for(int j=(i==lqi?lqj+1:0);j<chess[0].size();j++)
+        int j=lqj+1;
+        if(i>lqi)
         {
-            if(chess[i][j]==false)
+            j=0;
+        }
+        for(;j<chess[0].size();j++)
+        {
+            if(chess[i][j]==false&&isQueenSafe(chess,i,j))
             {
                 chess[i][j]=true;
                 nqueen1(chess,cq+1,i,j,asf+to_string(i)+to_string(j)+"-");
