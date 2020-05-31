373baf0cfe3e8894b1919c0b72ec1eeaa1bfd418
manishyadavv <manishyadav.nd@gmail.com>
2020-06-01 02:04:04 +0530

    GFG trying nqueen but require more optimised appraoch

---
+++ GFG{trying}/NQueen_Problem.cpp
@@ -0,0 +1,110 @@
+//
+// Created by MANISH YADAV on 01/06/20.
+//
+
+//https://practice.geeksforgeeks.org/problems/n-queen-problem/0
+
+
+#include<iostream>
+#include<string>
+#include "vector"
+using namespace std;
+
+
+bool isQueenSafe(vector<vector<bool>>& chess,int row,int col)
+{
+    for(int i=col-1;i>=0;i--)
+    {
+        if(chess[row][i]==true)
+        {
+            return false;
+        }
+    }
+
+    for(int i=row-1;i>=0;i--)
+    {
+        if(chess[i][col]==true)
+        {
+            return false;
+        }
+    }
+    //pichla diagonal
+    for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--)
+    {
+        if(chess[i][j]==true)
+        {
+            return false;
+        }
+    }
+    //agla diagonal
+    for(int i=row-1,j=col+1;i>=0&&j<chess[0].size();i--,j++)
+    {
+        if(chess[i][j]==true)
+        {
+            return false;
+        }
+    }
+
+
+    return true;
+
+
+}
+
+//cq=current queen
+//lqi last queen i last queen j
+void nqueen1(vector<vector<bool>>& chess,int cq,int lqi,int lqj,string asf)
+{
+
+    if(cq==chess.size())
+    {
+        if(asf=="")
+            cout<<"-1";
+        else
+        cout<<"["<<asf<<"]";
+        return;
+    }
+
+
+
+    for(int i=lqi;i<chess.size();i++)
+    {
+        int j=lqj+1;
+        if(i>lqi)
+        {
+            j=0;
+        }
+        for(;j<chess[0].size();j++)
+        {
+            if(chess[i][j]==false&&isQueenSafe(chess,i,j))
+            {
+                chess[i][j]=true;
+                nqueen1(chess,cq+1,i,j,asf+to_string(j+1)+" ");
+                chess[i][j]=false;
+            }
+        }
+    }
+
+}
+
+
+
+
+int main()
+{
+
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<bool>> chess (n,vector<bool>(n,false));
+        nqueen1(chess,0,0,-1,"");
+    }
+
+
+    return 0;
+
+}
